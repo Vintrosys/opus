8f37036eaeac84521ae81696bd724e8a2a4e288a
opusinfiniti <opusinfiniti2026@gmail.com>
2026-04-17 20:28:23 +0530

    added fixtures commit

---
+++ opus/opus/doctype/rewinding_machine_production_record/test_rewinding_machine_production_record.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Opus and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRewindingMachineProductionRecord(FrappeTestCase):
+	pass
