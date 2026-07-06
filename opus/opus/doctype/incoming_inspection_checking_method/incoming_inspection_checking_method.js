8a1e7328af26dd5586ce0248f648fe85e0d77645
opusinfiniti-git <opusinfiniti2026@gmail.com>
2026-07-06 09:57:27 +0530

    added fixtures commit

---
+++ opus/opus/doctype/incoming_inspection_checking_method/incoming_inspection_checking_method.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Opus and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Incoming Inspection Checking Method", {
+// 	refresh(frm) {
+
+// 	},
+// });
